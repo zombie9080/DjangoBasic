48935ddb5a3e3d66a8c0b61570d1ee318b2ddd10
G.Liu <avalon852456@gmail.com>
2018-02-05 16:45:16 +0800

    test

---
+++ DjangoBasic/Book/templatetags/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+_Author_ = 'G.Liu'
---
+++ DjangoBasic/Book/templatetags/base_filter.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+_Author_ = 'G.Liu'
+from django.template.library import Library
+
+rigister=Library()
+
+def page_filter(current_index,index)
