ef4676f4c0d7aafb0de6232a4866b55cd0d04c82
G.Liu <avalon852456@gmail.com>
2018-02-05 16:09:41 +0800

    add ulr configuration file to app Book

--- DjangoBasic/DjangoBasic/settings.py
+++ DjangoBasic/DjangoBasic/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Book'
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,7 @@ ROOT_URLCONF = 'DjangoBasic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
